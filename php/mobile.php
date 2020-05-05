67e57a2e271100c1dc88ac1fd6d92fafd7f4450b
Leonid Tsaruk <tsaruk-2015@mail.ru>
2020-05-05 18:29:50 +0300

    Summary

--- php/mobile.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$OS = getOS();
-
-if($OS == 'Android' || $OS == 'iPhone' || $OS == 'iPad' || $OS == 'BlackBerry' || $OS == 'Mobile'){
-	echo '<script>document.location.href = "http://leoltd.zzz.com.ua/mobile.html";</script>';
-}
-
-?>
--- script/timer/preloader.js
+++
@@ -1,7 +0,0 @@
-var preloader = document.getElementsByClassName('indexload_animation')[0];
-
-preloader.style = 'height: ' + window.innerHeight + 'px; width: ' + window.innerWidth + 'px;';
-function preloaderAnimation(){
-	$("#indexload_animation").fadeOut(1000);
-}
-setTimeout(preloaderAnimation, 100);
